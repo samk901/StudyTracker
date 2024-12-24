0e2bf2e65515e16103c11a8cf46c25e0491a47be
Samuel Kim <samuelkim@Samuels-Air.attlocal.net>
2024-12-24 08:09:17 -0500

    Added endpoints for lc questions, installed swagger

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"time"
 
 	"github.com/gin-gonic/gin"
 )
@@ -10,6 +11,24 @@ import (
 type User struct {
 	ID   string `json:"id"`
 	Name string `json:"name"`
+}
+
+type Difficulty string
+
+const (
+	Easy   Difficulty = "Easy"
+	Medium Difficulty = "Medium"
+	Hard   Difficulty = "Hard"
+)
+
+type LeetCodeQuestions struct {
+	ID                string     `json:"id"`
+	Name              string     `json:"name"`
+	Pattern           string     `json:"pattern"`
+	Difficulty        Difficulty `json:"difficulty"`
+	LastCompletedTime time.Time  `json:"lastCompletedTime"`
+	NextDueTime       time.Time  `json:"nextDueTime"`
+	Notes             string     `json:"notes"`
 }
 
 func main() {
@@ -24,6 +43,8 @@ func main() {
 	{
 		v1.GET("/users", getUsers)
 		v1.POST("/users", createUser)
+		v1.GET("/questions", getQuestions)
+		v1.POST("/questions", createQuestion)
 	}
 
 	router.Run()
@@ -48,4 +69,25 @@ func createUser(c *gin.Context) {
 	fmt.Println("Hello World!")
 
 	c.JSON(http.StatusCreated, newUser)
+}
+
+func getQuestions(c *gin.Context) {
+	c.JSON(http.StatusOK, gin.H{
+		"questions": []LeetCodeQuestions{
+			{ID: "1", Name: "Two Sum", Pattern: "Array", Difficulty: Easy, LastCompletedTime: time.Now(), NextDueTime: time.Now(), Notes: "Notes"},
+		},
+	})
+}
+
+func createQuestion(c *gin.Context) {
+	var newQuestion LeetCodeQuestions
+	if err := c.BindJSON(&newQuestion); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+
+	// Add Question logic here
+	fmt.Println("Created a new question!")
+
+	c.JSON(http.StatusCreated, newQuestion)
 }
